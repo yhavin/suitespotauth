1acc781ca2b7fd8dc50ce0f6052b7c42b9d2a708
yhavin <y.havin@gmail.com>
2024-02-21 12:48:08 -0500

    Add credentials CLI logic

---
+++ suitespotauth/configure.py
@@ -0,0 +1,32 @@
+import getpass
+
+import keyring
+
+
+def mask_username(username, show_start=1, show_end=6):
+    """Utility function to mask saved username."""
+    if len(username) > (show_start + show_end):
+        return username[:show_start] + "*" * (len(username) - (show_start + show_end)) + username[-show_end:]
+    else:
+        return username[:show_start] + "*" * (len(username) - show_start)
+
+
+def main():
+    """Prompt and store SuiteSpot credentials."""
+    service = "suitespotauth"
+
+    stored_username = keyring.get_password(service, "username")
+    if stored_username:
+        masked_username = mask_username(stored_username, show_start=1, show_end=6)
+        username = input(f"SuiteSpot username [{masked_username}]: ") or stored_username
+    else:
+        username = input("SuiteSpot username: ")
+
+    stored_password = keyring.get_password(service, "password")
+    if stored_password:
+        password = getpass.getpass("SuiteSpot password: [Enter to keep current]: ") or stored_password
+    else:
+        password = getpass.getpass("SuiteSpot password [Input hidden]: ")
+
+    keyring.set_password("suitespotauth", "username", username)
+    keyring.set_password("suitespotauth", "password", password)
